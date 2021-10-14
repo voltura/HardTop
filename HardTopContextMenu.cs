a5609b12481c36fdd4b30a227621a58bb05d95dd
Joakim Skoglund <joakim@voltura.se>
2021-10-15 00:21:36 +0200

    Add project files.

---
+++ HardTopContextMenu.cs
@@ -0,0 +1,150 @@
+﻿#region Using statements
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+#endregion Using statements
+
+namespace HardTop
+{
+    internal class HardTopContextMenu : IDisposable
+    {
+
+        #region Internal context menu
+
+        internal ContextMenu ContextMenu { get; private set; }
+        internal const int NUMBER_OF_FIXED_ITEMS = 7;
+
+        #endregion Internal context menu
+
+        #region Internal contructor
+
+        internal HardTopContextMenu()
+        {
+            CreateContextMenu();
+            AddUpdatedWindowsInfoToContextMenu();
+            ContextMenu.Collapse += ContextMenu_Collapse;
+            ContextMenu.Popup += ContextMenu_Popup;
+        }
+
+        #endregion Internal constructor
+
+        #region Event handling
+
+        private void ContextMenu_Popup(object sender, EventArgs e)
+        {
+            AddUpdatedWindowsInfoToContextMenu();
+        }
+
+        private void ContextMenu_Collapse(object sender, EventArgs e)
+        {
+            AddUpdatedWindowsInfoToContextMenu();
+        }
+
+        private void WindowItem_Click(object o, EventArgs e)
+        {
+            var mi = (MenuItem)o;
+            mi.Enabled = false;
+            mi.Checked = !mi.Checked;
+            NativeMethods.ToogleWindowAlwaysOnTop((IntPtr)mi.Tag, mi.Checked);
+            EnableMenuItem(mi);
+        }
+
+        private static void ExitMenuClick(object o, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void StartWithWindowsClick(object o, EventArgs e)
+        {
+            try
+            {
+                MenuItem mi = (MenuItem)o;
+                mi.Enabled = false;
+                mi.Checked = !mi.Checked;
+                Settings.StartWithWindows = mi.Checked;
+                EnableMenuItem(mi);
+            }
+            catch (Exception ex)
+            {
+                Message.Show(Resources.FailedToUpdateRegistry, ex);
+            }
+        }
+
+        private void AboutClick(object o, EventArgs e)
+        {
+            MenuItem mi = (MenuItem)o;
+            mi.Enabled = false;
+            Message.Show(Resources.About);
+            EnableMenuItem(mi);
+        }
+
+        #endregion Event handling
+
+        #region Private method for context menu creation
+
+        private void CreateContextMenu()
+        {
+            ContextMenu = new ContextMenu(new MenuItem[NUMBER_OF_FIXED_ITEMS]
+            {
+                new MenuItem(Resources.AboutMenu, AboutClick),
+                new MenuItem(Resources.SeparatorMenu),
+                new MenuItem(Resources.StartWithWindowsMenu, StartWithWindowsClick) { Checked = Settings.StartWithWindows },
+                new MenuItem(Resources.SeparatorMenu),
+                new MenuItem(Resources.ExitMenu, ExitMenuClick),
+                new MenuItem("Windows; checked equals \"always on top\" - click to toogle:") { DefaultItem = true, BarBreak = true },
+                new MenuItem(Resources.SeparatorMenu)
+            });
+        }
+
+        #endregion Private method for context menu creation
+
+        #region Private helper methods for menu items
+
+        private void AddUpdatedWindowsInfoToContextMenu()
+        {
+            while (ContextMenu.MenuItems.Count > NUMBER_OF_FIXED_ITEMS) ContextMenu.MenuItems.RemoveAt(NUMBER_OF_FIXED_ITEMS);
+            NativeMethods.GetDesktopWindowHandlesAndTitles(out List<IntPtr> handles, out List<string> titles);
+            List<string> ignoreTheseWindows = new List<string>() { "Program Manager", "MainWindow" };
+            for (int i = 0; i < titles.Count; i++)
+                if (!ignoreTheseWindows.Contains(titles[i]))
+                    ContextMenu.MenuItems.Add(new MenuItem(titles[i], WindowItem_Click) { Name = titles[i], Tag = handles?[i], Checked = NativeMethods.AlwaysOnTopWindows().Contains((IntPtr)handles?[i]) });
+        }
+
+        private static void EnableMenuItem(MenuItem mi)
+        {
+            if (mi != null) mi.Enabled = true;
+        }
+
+        #endregion Private helper methods for menu items
+
+        #region IDisposable methods
+
+        /// <summary>
+        /// Disposes the context menu
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing)
+            {
+                return;
+            }
+            CleanupContextMenu();
+        }
+
+        private void CleanupContextMenu()
+        {
+            ContextMenu?.Dispose();
+            ContextMenu = null;
+        }
+
+        #endregion IDisposable methods
+    }
+}
---
+++ NativeMethods.cs
@@ -0,0 +1,182 @@
+﻿#region Using statements
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+#endregion Using statements
+
+namespace HardTop
+{
+    internal static class NativeMethods
+    {
+        #region Private constants used in user32 API's interaction
+
+        private const int WS_EX_TOPMOST = 0x00000008;
+        private const int GWL_EXSTYLE = -20;
+
+        #endregion Private constants used in user32 API's interaction
+
+        #region Private variables
+
+        private static List<IntPtr> WindowHandles;
+        private static List<string> WindowTitles;
+
+        #endregion Private variables
+
+        #region Delegates used in user32 API's interaction
+
+        private delegate bool EnumWindowsProc(IntPtr hwnd, ArrayList lParam);
+        private delegate bool EnumDelegate(IntPtr hWnd, int lParam);
+
+        #endregion Delegates used in user32 API's interaction
+
+        #region Internal functions for Window handling
+
+        internal static void GetDesktopWindowHandlesAndTitles(out List<IntPtr> handles, out List<string> titles)
+        {
+            WindowHandles = new List<IntPtr>();
+            WindowTitles = new List<string>();
+            if (!EnumDesktopWindows(IntPtr.Zero, GetDesktopWindowHandlesAndTitlesFilterCallback, IntPtr.Zero))
+            {
+                handles = null;
+                titles = null;
+            }
+            else
+            {
+                handles = WindowHandles;
+                titles = WindowTitles;
+            }
+        }
+
+        internal static ArrayList AlwaysOnTopWindows()
+        {
+            var topmostWindowHandles = new ArrayList();
+            EnumWindows(EnumWindowsCallback, topmostWindowHandles);
+            return topmostWindowHandles;
+        }
+
+        internal static void ToogleWindowAlwaysOnTop(IntPtr hwnd, bool alwaysOnTop)
+        {
+            SetWindowPos(hwnd, (IntPtr)(alwaysOnTop ? -1 : -2), 0, 0, 0, 0, 67);
+        }
+
+        #endregion Internal functions for Window handling
+
+        #region Private Callback methods; window handling
+
+        private static bool EnumWindowsCallback(IntPtr hWnd, ArrayList lParam)
+        {
+            int exStyle = GetWindowLong(hWnd, GWL_EXSTYLE);
+            if ((exStyle & WS_EX_TOPMOST) == WS_EX_TOPMOST) lParam.Add(hWnd);
+            return true;
+        }
+
+        private static bool GetDesktopWindowHandlesAndTitlesFilterCallback(IntPtr hWnd, int lParam)
+        {
+            StringBuilder sb_title = new StringBuilder(1024);
+            GetWindowText(hWnd, sb_title, sb_title.Capacity);
+            string title = sb_title.ToString();
+            if (IsWindowVisible(hWnd) && string.IsNullOrEmpty(title) == false)
+            {
+                WindowHandles.Add(hWnd);
+                WindowTitles.Add(title);
+            }
+            return true;
+        }
+
+        #endregion Private Callback methods; window handling
+
+        #region user32.dll function imports; window handling
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, ArrayList lParam);
+
+        [DllImport("user32.dll")]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int xy, uint flagsw);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowText",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern int GetWindowText(IntPtr hWnd, StringBuilder lpWindowText, int nMaxCount);
+
+        [DllImport("user32.dll", EntryPoint = "EnumDesktopWindows",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool EnumDesktopWindows(IntPtr hDesktop, EnumDelegate lpEnumCallbackFunction, IntPtr lParam);
+
+        #endregion user32.dll function imports; window handling
+
+        #region user32.dll function import; used to free GDI+ icon from memory
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool DestroyIcon(IntPtr handle);
+
+        #endregion user32.dll function import; used to free GDI+ icon from memory
+
+        #region Unused code snippets
+        /*
+        const int WS_VISIBLE = 0x10000000;
+        const int GWL_STYLE = -16;
+
+            var processesToKill = new HashSet<uint>();
+            foreach (IntPtr hWnd in topmostWindowHandles)
+            {
+                uint processId = 0;
+                GetWindowThreadProcessId(hWnd, out processId);
+                processesToKill.Add(processId);
+            }
+
+            foreach (uint pid in processesToKill)
+            {
+                Process proc = Process.GetProcessById((int)pid);
+                Console.WriteLine("Killing " + proc.ProcessName);
+                // kill process, except explorer.exe
+            }
+                static bool EnumWindowsCallback(IntPtr hWnd, ArrayList lParam)
+        {
+            int exStyle = GetWindowLong(hWnd, GWL_EXSTYLE);
+            int style = GetWindowLong(hWnd, GWL_STYLE);
+            if ((exStyle & WS_EX_TOPMOST) == WS_EX_TOPMOST
+                && (style & WS_VISIBLE) == WS_VISIBLE
+                )
+            {
+                lParam.Add(hWnd);
+            }
+            return true;
+        }
+
+         */
+        /*[DllImport("user32.dll")]
+internal static extern IntPtr GetForegroundWindow();
+
+[DllImport("user32.dll")]
+internal static extern IntPtr GetTopWindow(IntPtr hWnd);
+*/
+        /*      
+      [DllImport("Kernel32.dll")] 
+      public static extern IntPtr GetConsoleWindow();
+
+      internal delegate bool WindowEnumCallback(int hwnd, int lparam);
+
+      [DllImport("user32.dll")]
+      [return: MarshalAs(UnmanagedType.Bool)]
+      internal static extern bool EnumWindows(WindowEnumCallback lpEnumFunc, int lParam);
+*/
+
+        #endregion Unused code snippets
+    }
+}
